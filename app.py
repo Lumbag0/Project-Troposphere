09efe166330c19c6ce7d11bcadbb7e3b388ebaab
lumbag0 <ecape0207@gmail.com>
2025-03-31 08:20:05 -0600

    Created a user input method for getting API key

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import map
+import requests
 
 # App class
 class App:
@@ -6,3 +7,19 @@ class App:
         self.app = root
         self.app.title("Project Troposphere")
         map.Map()
+
+    # Description: Gets the key from the user to use to authenticate with Weatherbit.io
+    # Parameters: N\A
+    # Returns: String
+    def get_key() -> str:
+        valid_key = False
+        # Loop until valid key is entered
+        while not valid_key:
+            key = input("Please enter the API Key for Weatherbit.io: ")
+            response = requests.get(f"https://api.weatherbit.io/v2.0/current?city=Denver&key={key}")
+            # If the response is 200 (ok) then break out of loop, otherwise print error
+            if response.status_code == 200:
+                valid_key = True
+            else:
+                print(f"ERROR: Invalid key {key}")
+        return key
--- project-troposphere.py
+++ project-troposphere.py
@@ -1,7 +1,12 @@
+# Description: Main program that starts the map
+
 import tkinter as tk
-import app
+from app import App
 
 if __name__ == "__main__":
+    # Get API key and store it here
+    key = App.get_key()
+    
     root = tk.Tk()
-    app = app.App(root)
+    app = App(root)
     root.mainloop()
